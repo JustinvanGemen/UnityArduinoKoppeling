0b4b732181fa003ffbf57ef3459e22bb545949bf
Justin van Gemen <20277@ma-web.nl>
2016-10-28 09:05:43 +0100

    Everything
    *Unity Script
    *Tekst document with Codebender Script

---
+++ Assets/ArduinoScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+public class ArduinoScript : MonoBehaviour
+{
+    SerialPort sp = new SerialPort("COM5", 9600);
+    float[] lastRot = { 0, 0, 0 };
+
+    void Awake()
+    {
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+    void Update()
+    {
+        try
+        {
+            string value = sp.ReadLine();
+            string[] vector3 = value.Split(',');
+            if (vector3[0] != "" && vector3[1] != "")
+            {
+                transform.Rotate(float.Parse(vector3[0]) - lastRot[0], float.Parse(vector3[1]) - lastRot[1],  0f,  Space.Self);
+                lastRot[0] = float.Parse(vector3[0]);
+                lastRot[1] = float.Parse(vector3[1]);
+                sp.BaseStream.Flush();
+            }
+            for (int i = 0; i < vector3.Length; i++)
+            {
+                Debug.Log(vector3[i]);
+            }
+        }
+        catch (System.Exception) {}
+    }
+}
